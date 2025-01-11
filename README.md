a590c6c393936ea36ff7af4bd8b486ee316ce154
kojima10111 <153720806+kojima10111@users.noreply.github.com>
2025-01-11 20:46:36 +0900

    first pr (#1)
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * Add additional setup steps to README
    
    * Add finger_alphabet_images folder
    
    * a
    
    * Delete .DS_Store
    
    ---------
    
    Co-authored-by: kojima10111 <k.kojima10111@gmail.com>

--- README.md
+++ README.md
@@ -2,8 +2,25 @@
 # akari_finger_alphabet_game
 
 ## セットアップ方法
-
-## 起動方法
-
+1. 指文字画像をSDカードへコピー  
+   `finger_alphabet_images`に入っている指文字の各画像をAkari内のSDカードの`jpg/`にコピーする
+2. ローカルにクローンする  
+   `cd ~`  
+   `git clone https://github.com/AkariGroup/akari_finger_alphabet_game.git`  
+   `cd akari_finger_alphabet_game`  
+3. 仮想環境の作成  
+   `python3 -m venv venv`  
+   `. venv/bin/activate`  
+   `pip install -r requirements.txt`  
+## 起動方法  
+1. 仮想環境の有効化  
+   `.venv/bin/activate`  
+2. `python3 game_practice.py`を実行すると、`game_practice.py`が開始する  
+   練習モードをスキップする場合は、`python3 game_main.py`を実行し、手順6へ  
+3. Akariの画面に表示されている指文字を真似して、指文字を練習する  
+4. 正しく指文字が表せたら、次の指文字が表示される  
+5. 「あいうえお」全ての練習が終わると、`game_main.py`が開始する  
+6. Akariの画面に表示される文字の母音を数え、最も多く含まれる母音を指文字で表す  
+7. AKARI_MAINウィンドウを選択した状態でキーボードの`q`キーを押すと終了する  
 ## その他
-このアプリケーションは愛知工業大学 情報科学部 知的制御研究室により作成されたものです。  
+このアプリケーションは愛知工業大学 情報科学部 知的制御研究室により作成されたものです
---
+++ game_practice.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python3
+import game_main
+import argparse
+import cv2
+from lib.akari_yolo_lib.oakd_yolo import OakdYolo
+from akari_client import AkariClient
+
+
+def main() -> None:
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-m",
+        "--model",
+        help="Provide model name or model path for inference",
+        default="model/best_openvino_2022.1_6shave.blob",
+        type=str,
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        help="Provide config path for inference",
+        default="json/best.json",
+        type=str,
+    )
+    parser.add_argument(
+        "-f",
+        "--fps",
+        help="Camera frame fps. This should be smaller than nn inference fps",
+        default=10,
+        type=int,
+    )
+    args = parser.parse_args()
+
+    end = False
+
+
+    display = 0
+
+    # カウントの閾値　この回数以上認識すると各文字として判定
+    a_THRESHOLD = 15
+    i_THRESHOLD = 15
+    u_THRESHOLD = 15
+    e_THRESHOLD = 15
+    o_THRESHOLD = 15
+
+    # カウントのための変数
+    num_a = 0
+    num_i = 0
+    num_u = 0
+    num_e = 0
+    num_o = 0
+    i=0
+
+    # AKARIのSDカード上に保存されている練習用画像のパス
+    images = ["/jpg/handA.jpg","/jpg/handI.jpg","/jpg/handU.jpg","/jpg/handE.jpg","/jpg/handO.jpg"]
+
+    while not end:
+        oakd_yolo = OakdYolo(args.config, args.model, args.fps)
+        while True:
+            frame = None
+            detections = []
+            try:
+                frame, detections = oakd_yolo.get_frame()
+            except BaseException:
+                print("===================")
+                print("get_frame() error! Reboot OAK-D.")
+                print("If reboot occur frequently, Bandwidth may be too much.")
+                print("Please lower FPS.")
+                print("==================")
+                break
+
+            akari = AkariClient()
+            m5 = akari.m5stack
+            if display==0:
+                # 練習用の指文字画像表示
+                m5.set_display_image(images[i])
+                display = 1
+
+                
+            if frame is not None:
+                if detections:
+                    for detection in detections:
+                        
+                        # 認識したらカウントを増やす
+                        if detection.label == 0:
+                            num_a += 1
+                        elif detection.label == 1:
+                            num_i += 1
+                        elif detection.label == 2:
+                            num_u += 1
+                        elif detection.label == 3:
+                            num_e += 1
+                        elif detection.label == 4:
+                            num_o += 1
+                        
+                        # カウントが閾値を超え、かつ画像と一致していたら次の画像へ
+                        if num_a > a_THRESHOLD and i == 0:
+                            display = 0
+                            i+=1
+                        elif num_i > i_THRESHOLD and i == 1:
+                            display = 0
+                            i+=1
+                        elif num_u > u_THRESHOLD and i == 2:
+                            display = 0
+                            i+=1
+                        elif num_e > e_THRESHOLD and i == 3:
+                            display = 0
+                            i+=1
+                        elif num_o > o_THRESHOLD and i == 4:
+                            display = 0
+                            end = True
+                            oakd_yolo.close()
+                            game_main.main()
+                else:
+                    # 指文字の認識カウントをリセット
+                    num_a = 0
+                    num_i = 0
+                    num_u = 0
+                    num_e = 0
+                    num_o = 0
+            
+                oakd_yolo.display_frame("AKARI_PRACTICE", frame, detections)
+
+
+            if cv2.waitKey(1) == ord("q"):
+                end = True
+                break
+        oakd_yolo.close()
+
+if __name__ == "__main__":
+    main()
